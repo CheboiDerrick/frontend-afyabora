dba58889d848f28c560577f0dca9c370ea9d4115
Cheboi Derrick <derrick.cheboi@student.moringaschool.com>
2021-11-03 11:04:30 +0300

    view doctors (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DoctorsComponent
     HomeComponent
   ],
   imports: [
